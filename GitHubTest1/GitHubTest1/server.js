6cc8d7c13bfcd5597c8b16cec233301ba44db84b
Sam Lee <leeseunghee1125@gmail.com>
2018-04-02 16:23:17 +0800

    server.js commit

--- GitHubTest1/GitHubTest1/server.js
+++ GitHubTest1/GitHubTest1/server.js
@@ -1,8 +1,10 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 3000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+}).listen(port, function () {
+    console.log('Server Started On Port 3000!')
+});
